d395e84b01d8cfb3629a6f8ec3354836487f25ce
段炼 <dll-618@126.com>
2023-12-14 17:24:43 +0800

    删除测试方法

--- src/Interfaces/IAutoDoor.cs
+++ src/Interfaces/IAutoDoor.cs
@@ -4,8 +4,6 @@ namespace IZU.Interfaces
 {
     public interface IAutoDoor : IDevice, ICanStart, ICanStop, ICanOpen, ICanClose, IEmergency, IReset, IInitial, ISwitch
     {
-        Task<string> CloseManualAsync1();
-        Task<string> OpenManualAsync1();
 
     }
 }
