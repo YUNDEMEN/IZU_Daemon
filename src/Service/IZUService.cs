6c08480f3c656831b0aa1b3797b8ba8bd850f6f2
段炼 <dll-618@126.com>
2024-01-24 13:24:00 +0800

    .

--- src/Service/IZUService.cs
+++ src/Service/IZUService.cs
@@ -31,15 +31,6 @@ namespace IZU.Service
         }
         public async Task StartAsync()
         {
-            Task.Factory.StartNew(() => {
-                int i = 0;
-                while (true)
-                {
-                    bool rec = i % 2 == 0;
-                    _logger.LogInformation($"{(rec?"connected":"disconnected")}");
-                    i++;
-                }
-            });
             //var _loggers = IZULogging.Factory.CreateLogger<Device>();
             _logger.LogInformation("---------------IZU service starting---------------");
             IZUConfig.Read();
