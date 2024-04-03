5c09827f9b5b7179e78853b39476a4246e3d056a
段炼(Dezi) <dll-618@126.com>
2024-04-03 12:27:28 +0800

    安装时将端口设置为8031

--- service-install.bat
+++ service-install.bat
@@ -1,3 +1,3 @@
 @echo off
-sc create izu-daemon binPath="%~dp0IZU-Service.exe --urls http://127.0.0.1:6000" start= auto
+sc create izu-daemon binPath="%~dp0IZU-Service.exe --urls http://127.0.0.1:8031" start= auto
 pause
